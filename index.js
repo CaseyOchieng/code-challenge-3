b7473f35d9333f77cdee42230e4b131dfffe2e4e
Casey Ochieng <caseyochieng46@gmail.com>
2022-12-14 10:16:21 +0300

    completed assignment

--- index.js
+++ index.js
@@ -183,11 +183,10 @@ function getMovies() {
 
   let list = document.getElementById("movie-list");
 
-  for (let i of movies) {
-   
-    const content = `
+  movies.map(movie =>{
+const content = `
   
-  <nav class="Hands">F</nav>
+  <nav class="Hands"></nav>
     <div class="card">
   <div class="card-header" id="heading">
     <h5 class="mb-0">   
@@ -196,11 +195,11 @@ function getMovies() {
  <div id="collapse" class="collapse show" aria-labelledby="heading" data-parent="#accordion">
     <div class="card-body">
 
-      <h5>${i.title}</h5>
-      <p>[Description] :${i.description}</p>
-      <p>[Time]:${i.showtime}</p>
-      <p>[Capacity]${i.capacity}</p>
-      <img src=${i.poster} width="200px" height="200px">
+      <h5>${movie.title}</h5>
+      <p>[Description] :${movie.description}</p>
+      <p>[Time]:${movie.showtime}</p>
+      <p>[Capacity]${movie.capacity}</p>
+      <img src=${movie.poster} width="200px" height="200px">
       <br> 
       <br>
       <div class="pro" >
@@ -215,16 +214,32 @@ function getMovies() {
   </div>
 </div>
 `;
+list.insertAdjacentHTML("afterbegin", content);
 // hiding the unbook button
-function showElement() {
-  element = document.querySelector('.btn btn-link');
-  element.style.display = 'block';  
+const unbook = list.querySelector('.btn-unlink');
+
+unbook.style.display="none"
+const book = list.querySelector('.btn-link');
+function bookticket() {
+   
+  unbook.style.display = 'block'; 
+  book.style.display = "none" 
 }
 
-function hideElement() {
-  element = document.querySelector('.btn btn-unlink');
-  element.style.display = 'none';
+
+function unbookticket() {
+ 
+  book.style.display = 'block';
+  unbook.style.display = "none"
 }
+book.addEventListener("click", bookticket)
+
+unbook.addEventListener("click", unbookticket)
+
+
+  })
+   
+} 
 // hiding the book button
 
 
@@ -234,9 +249,9 @@ function hideElement() {
 
 
     // Append newly created card element to the container
-    list.innerHTML += content;
-  }
-}
+    
+  
+
 
 
 
